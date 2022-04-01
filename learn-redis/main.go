be959266932938badbb771dbf30023b9b72a764b
Andika Nugraha <m.andika.nugraha@gmail.com>
2022-04-01 15:42:20 +0700

    redis key value operation

--- learn-redis/main.go
+++ learn-redis/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -17,6 +18,9 @@ type user struct {
 
 // constant of time
 const TimeFiveMinutes = time.Second * 300
+
+// redis connection for main package variable
+var c redis.Conn
 
 // create redis pool connection
 func newRedisPool(host, password string) *redis.Pool {
@@ -45,7 +49,7 @@ func main() {
 	pool := newRedisPool("localhost:6379", "my_pass")
 
 	// get redis connection
-	c := pool.Get()
+	c = pool.Get()
 
 	// grcefully close redis connection
 	defer c.Close()
@@ -57,11 +61,17 @@ func main() {
 	}
 	log.Println(pong)
 
-	// HSET redis for store and get Hash datatype on redis
-	// doc for HSET: https://redis.io/commands/hset
+	hashOperation()
+	setOperation()
 
+}
+
+// HSET redis for store and get Hash datatype on redis
+// doc for HSET: https://redis.io/commands/hset
+func hashOperation() {
+	fmt.Println("Hash Operation")
 	// store data using HSET command
-	_, err = c.Do("HSET", "user:1", "first_name", "andika", "last_name", "nugraha", "birth_year", 1995, "gender", "male")
+	_, err := c.Do("HSET", "user:1", "first_name", "andika", "last_name", "nugraha", "birth_year", 1995, "gender", "male")
 	if err != nil {
 		log.Printf("fail store data to redis: %s\n", err.Error())
 	}
@@ -71,14 +81,14 @@ func main() {
 	if err != nil {
 		log.Printf("fail get data from redis: %s\n", err.Error())
 	}
-	log.Println(firstName)
+	fmt.Println(firstName)
 
 	// get all hash value to map using HGETALL
 	userMap, err := redis.StringMap(c.Do("HGETALL", "user:1"))
 	if err != nil {
 		log.Printf("fail get data from redis: %s\n", err.Error())
 	}
-	log.Println(userMap)
+	fmt.Println(userMap)
 
 	// get all hash value to struct using HGETALL
 	var userStruct user
@@ -91,5 +101,57 @@ func main() {
 		log.Printf("fail to scan value into struct: %s\n", err.Error())
 	}
 	fmt.Println(userStruct)
-	fmt.Printf("%+v", userStruct)
+	fmt.Printf("%+v\n\n", userStruct)
+}
+
+// SET redis for store and get key-value datatype on redis
+// doc for SET: https://redis.io/commands/set
+func setOperation() {
+	fmt.Println("Set Operation")
+
+	// store data using SET command
+	_, err := c.Do("SET", "instagram_username", "andikaaa.nugraha")
+	if err != nil {
+		log.Printf("fail store data to redis: %s\n", err.Error())
+	}
+
+	// get data using GET command
+	data, err := redis.String(c.Do("GET", "instagram_username"))
+	if err != nil {
+		log.Printf("fail get data from redis: %s\n", err.Error())
+	}
+	fmt.Println(data)
+
+	// set with struct data
+	userStruct := user{
+		FirstName: "andika",
+		LastName:  "nugraha",
+		BirthYear: 1995,
+		Gender:    "male",
+	}
+
+	// marshaling data into []byte
+	userBytes, err := json.Marshal(userStruct)
+	if err != nil {
+		log.Printf("fail to marshaling data: %s\n", err.Error())
+	}
+
+	// store data using SET
+	_, err = c.Do("SET", "user:1", userBytes)
+	if err != nil {
+		log.Printf("fail store data to redis: %s\n", err.Error())
+	}
+
+	// get data using GET commnand
+	var resultUserStruct user
+	dataStructByte, err := redis.String(c.Do("GET", "user:1"))
+	if err != nil {
+		log.Printf("fail get data from redis: %s\n", err.Error())
+	}
+	err = json.Unmarshal([]byte(dataStructByte), &resultUserStruct)
+	if err != nil {
+		log.Printf("fail to unmarshaling data: %s\n", err.Error())
+	}
+	fmt.Println(resultUserStruct)
+	fmt.Printf("%+v\n\n", resultUserStruct)
 }
