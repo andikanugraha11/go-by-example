e7c2b963b2305843cf4f44003a748ac93af2cb06
Andika Nugraha <m.andika.nugraha@gmail.com>
2022-04-01 14:10:51 +0700

    redis HASH data set and get

--- learn-redis/main.go
+++ learn-redis/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
+
+type user struct {
+	FirstName string `redis:"first_name"`
+	LastName  string `redis:"last_name"`
+	BirthYear int    `redis:"birth_year"`
+	Gender    string `redis:"gender"`
+}
 
 // constant of time
 const TimeFiveMinutes = time.Second * 300
@@ -43,9 +51,45 @@ func main() {
 	defer c.Close()
 
 	// test ping to redis
-	_, err := c.Do("PING")
+	pong, err := c.Do("PING")
 	if err != nil {
 		log.Fatalf("fail ping to redis: %s\n", err.Error())
 	}
+	log.Println(pong)
 
+	// HSET redis for store and get Hash datatype on redis
+	// doc for HSET: https://redis.io/commands/hset
+
+	// store data using HSET command
+	_, err = c.Do("HSET", "user:1", "first_name", "andika", "last_name", "nugraha", "birth_year", 1995, "gender", "male")
+	if err != nil {
+		log.Printf("fail store data to redis: %s\n", err.Error())
+	}
+
+	// get single hash value to string using HGET
+	firstName, err := redis.String(c.Do("HGET", "user:1", "first_name"))
+	if err != nil {
+		log.Printf("fail get data from redis: %s\n", err.Error())
+	}
+	log.Println(firstName)
+
+	// get all hash value to map using HGETALL
+	userMap, err := redis.StringMap(c.Do("HGETALL", "user:1"))
+	if err != nil {
+		log.Printf("fail get data from redis: %s\n", err.Error())
+	}
+	log.Println(userMap)
+
+	// get all hash value to struct using HGETALL
+	var userStruct user
+	res, err := redis.Values(c.Do("HGETALL", "user:1"))
+	if err != nil {
+		log.Printf("fail get data from redis: %s\n", err.Error())
+	}
+	err = redis.ScanStruct(res, &userStruct)
+	if err != nil {
+		log.Printf("fail to scan value into struct: %s\n", err.Error())
+	}
+	fmt.Println(userStruct)
+	fmt.Printf("%+v", userStruct)
 }
